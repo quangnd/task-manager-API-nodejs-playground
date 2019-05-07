939a89e61195030a71862e30ac74850264695484
mun <quangnd.edu@gmail.com>
2019-05-07 21:08:03 +0700

    Add logout and logoutAll API

--- src/models/User.js
+++ src/models/User.js
@@ -83,6 +83,7 @@ userSchema.pre("save", async function(next) {
 
   next();
 });
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const User = require("../models/user");
+const User = require("../models/User");
+const auth = require("../middleware/auth");
 const router = new express.Router();
 
 router.post("/users", async (req, res) => {
@@ -13,7 +14,13 @@ router.post("/users", async (req, res) => {
   }
 });
 
-router.get("/users", async (req, res) => {
+//Get profile
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user)
+});
+
+//Get list user
+router.get("/users", auth, async (req, res) => {
   try {
     const users = await User.find({});
     res.send(users);
@@ -90,4 +97,27 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(token => {
+      return token.token !== req.token
+    })
+    await req.user.save()
+
+    res.send()
+  } catch (err) {
+    res.status(500).send()
+  }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = []
+    await req.user.save()
+
+    res.send()
+  } catch (err) {
+    res.status(500).send()
+  }
+})
 module.exports = router;
