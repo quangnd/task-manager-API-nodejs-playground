f8c292bb78938083787866aea0fed235313040c4
mun <quangnd.edu@gmail.com>
2019-05-08 10:31:12 +0700

    Add sorting

--- src/routers/task-router.js
+++ src/routers/task-router.js
@@ -47,17 +47,30 @@ router.patch("/tasks/:id", auth, async (req, res) => {
 });
 
 //GET /tasks?completed=true
+//GET /tasks?limit=10&skip=20
+//GET /tasks?sortBy=createdAt:desc
 router.get("/tasks", auth, async (req, res) => {
   const match = {};
   if (req.query.completed) {
     match.completed = req.query.completed === 'true';
   }
 
+  const sort = {};
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':')
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+  }
+
   try {
     await req.user
       .populate({
         path: "tasks",
-        match
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+          sort
+        }
       })
       .execPopulate();
     res.send(req.user.tasks);
